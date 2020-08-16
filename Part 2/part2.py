f89d4ddd07f8571538970d3a83bfb06e73bc37c8
Bilal junaidy <61527086+BilalJunaidy@users.noreply.github.com>
2020-08-15 20:35:24 -0400

    Update part2.py

--- Part 2/part2.py
+++ Part 2/part2.py
@@ -1,23 +1,31 @@
-##OPENING AND READING PDF DOCUMENTS - INTRODUCTION TO THE FOLLOWING CONCEPTS:
-## 1. THE USE OF THE FILE CONTEXT MANAGER
-## 2. STREAM
-## 3. THE REASON FOR ADDING 'r' BEFORE ADDING THE FILE PATH
-## 4. VARIOUS METHODS OF WORKING WITH MULTI-LINE STRINGS IN PYTHON 
-## 5. OPENING THE FILE IN READ BINARY FORMAT AND NOT JUST READ
-## 6. UNDERSTANDING THE PDF READER OBJECT AND ITS MOST COMMON METHODS 
-## 7. UNDERSTANDING THE PDF PAGE OBJECT AND ITS MOST COMMON METHODS
-## 8. THE CHALLENGES OF EXTRACTING TEXT WITH PYPDF2 AND ALTERNATIVES
+##PART 2 - PYTHON FOR PDF TUTORIAL SERIES:
 
+##Code Explainer:
+##In the following line of code we are importing the PyPDF2 library (the one which we had
+##downloaded in the part 1 of this Tutorial. From this library we are specifically importing
+##the PdfFileReader Class which allows us to read pdf documents.
 
 from PyPDF2 import PdfFileReader as pdfreader
 
+##Code Explainer:
+##In the following line of codes, we are opening the "meetingminutes.pdf" pdf file and
+##referring to the file as "myfile", and this file is being opened using read binary method
+##because a pdf document has a binary format. 
+##"myfile" specifically, represents the bytes of information included in the
+##"meetingminutes.pdf" pdf document.
 
+with open(r'C:\Users\bilal\Desktop\Python PDF Tutorial\meetingminutes.pdf','rb') as myfile:
 
-with open(r'C:\Users\bilal\Desktop\Python PDF Tutorial\meetingminutes.pdf', 'rb') as file:
-    pdfreaderobject = pdfreader(file)
+##Code Explainer:
+##In the following line of code, we are passing the information stored into the "myfile" variable into
+##the pdfreader class. This pdfreader class will then return a pdfreaderobject, upon which we will be
+##able to perform various functions.
+##In simple words, this pdfreaderobject represents this "meetingminutes.pdf"
 
+    pdfreaderobject = pdfreader(myfile)
+##
     Number_of_pages = pdfreaderobject.getNumPages()
-    print(f'The number of pages are: {Number_of_pages}')
+    print(f'The number of pages are\n: \n{Number_of_pages}')
 
     global_information = pdfreaderobject.getDocumentInfo()
 
@@ -31,11 +39,17 @@ with open(r'C:\Users\bilal\Desktop\Python PDF Tutorial\meetingminutes.pdf', 'rb'
 
     print(f"This pdf is encrypted: {pdfreaderobject.isEncrypted}")
     
-    
-    first_page = pdfreaderobject.getPage(6)
-    first_page_text = first_page.extractText()
-    print(f"The following is the text on the first page : \n{first_page_text}")
+##Code Explainer:
+##Applying the getPage(i) where i represents the index of the page number you would like to get, returns
+##a pdfpageobject.
+##In simple words, a pdfpageobject can be thought of as representing a specific page in our
+##"meetingminutes.pdf" pdf document.
+
+    seventh_page = pdfreaderobject.getPage(6)
+    seventh_page_text = seventh_page.extractText()
+    print(f"The following is the text on the seveth page : \n{seventh_page_text}")
 
     for i in range(Number_of_pages):
         pageobject = pdfreaderobject.getPage(i)
         print(pageobject.extractText())
+
